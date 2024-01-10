51f296fc57130f7a6d7640123c5d0965e71c0378
Tomaz Treven <treven@nefta.io>
2024-01-10 15:37:24 +0100

    Added option to copy appId and Nuid to example integration

--- DirectIntegration/src/main/java/com/nefta/neftatestbed/MainActivity.java
+++ DirectIntegration/src/main/java/com/nefta/neftatestbed/MainActivity.java
@@ -1,8 +1,14 @@
 package com.nefta.neftatestbed;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,11 +19,15 @@ import com.nefta.sdk.NeftaPlugin;
 import com.nefta.neftatestbed.databinding.ActivityMainBinding;
 import com.nefta.sdk.Placement;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private NeftaPlugin _plugin;
+    private TextView _nuidText;
     private HashMap<Placement, PlacementController> _placementToControllers;
 
     @Override
@@ -27,8 +37,10 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        Context context = getApplicationContext();
+
         String appId = "5630785994358784";
-        _plugin = NeftaPlugin.Init(getApplicationContext(), appId);
+        _plugin = NeftaPlugin.Init(context, appId);
         _plugin.OnReady = this::OnReady;
         _plugin.OnBid = this::OnBid;
         _plugin.OnLoadStart = this::OnStartLoad;
@@ -47,7 +59,30 @@ public class MainActivity extends AppCompatActivity {
         } else {
             appIdText = "AppId: " + appId;
         }
-        ((TextView) findViewById(R.id.appId)).setText(appIdText);
+        TextView appIdTextView = findViewById(R.id.appId);
+        appIdTextView.setText(appIdText);
+        appIdTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("appId", appId);
+                clipboard.setPrimaryClip(clip);
+
+                Toast.makeText(context, R.string.app_id_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        _nuidText = findViewById(R.id.nuid);
+        _nuidText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("nuid", _nuidText.getText());
+                clipboard.setPrimaryClip(clip);
+
+                Toast.makeText(context, R.string.nuid_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -69,6 +104,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void OnReady(HashMap<String, Placement> placements) {
+        String userString = _plugin.GetToolboxUser();
+        try {
+            JSONObject user = new JSONObject(userString);
+            _nuidText.setText(user.getString("user_id"));
+        } catch (JSONException e) {
+            Log.i("DemoApp", "Error parsing toolbox user: " + e.getMessage());
+        }
+
         _placementToControllers.clear();
         ((LinearLayout)findViewById(R.id.placementContainer)).removeAllViews();
 
